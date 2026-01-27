828e252412976010ff728ff4f8be88ab4f981eb3
bremme <6715707+bremme@users.noreply.github.com>
2026-01-27 21:02:40 +0100

    replace dict constructor

--- src/shine2mqtt/__init__.py
+++ src/shine2mqtt/__init__.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 __version__ = version(__name__)
 
-PROJECT_URLS = dict(line.split(", ", 1) for line in metadata(__name__).get_all("Project-URL", []))
+PROJECT_URLS = {
+    k: v for k, v in (line.split(", ", 1) for line in metadata(__name__).get_all("Project-URL", []))
+}
 HOME_PAGE = PROJECT_URLS.get("Homepage")
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent.resolve()
