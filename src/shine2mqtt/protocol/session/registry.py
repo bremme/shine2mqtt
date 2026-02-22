5aac1b4699dd26587e2d53f62b69d7409fea4d9c
bremme <6715707+bremme@users.noreply.github.com>
2026-02-22 22:06:19 +0100

    Make override explicit

--- src/shine2mqtt/protocol/session/registry.py
+++ src/shine2mqtt/protocol/session/registry.py
@@ -1,3 +1,5 @@
+from typing import override
+
 from shine2mqtt.domain.interfaces.registry import SessionRegistry
 from shine2mqtt.domain.interfaces.session import Session
 from shine2mqtt.protocol.session.session import ProtocolSession
@@ -7,9 +9,11 @@ class ProtocolSessionRegistry(SessionRegistry):
     def __init__(self):
         self._sessions: dict[str, ProtocolSession] = {}
 
+    @override
     def get(self, datalogger_serial: str) -> Session | None:
         return self._sessions.get(datalogger_serial, None)
 
+    @override
     def get_all(self) -> list[Session]:
         return list(self._sessions.values())
 
