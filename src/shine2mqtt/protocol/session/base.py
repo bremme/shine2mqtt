dc407e5bd11018d89b03164f40b606eec55d845a
bremme <6715707+bremme@users.noreply.github.com>
2026-02-22 22:06:09 +0100

    Move frame reading knowledge here

--- src/shine2mqtt/protocol/session/base.py
+++ src/shine2mqtt/protocol/session/base.py
@@ -1,4 +1,5 @@
 from shine2mqtt.infrastructure.server.session import TCPSession
+from shine2mqtt.protocol.frame.constants import HEADER_LENGTH
 from shine2mqtt.protocol.frame.decoder import FrameDecoder
 from shine2mqtt.protocol.frame.encoder import FrameEncoder
 from shine2mqtt.protocol.messages.message import BaseMessage, DataloggerMessage
@@ -12,7 +13,7 @@ class BaseProtocolSession:
         self.decoder = decoder
 
     async def _read_message(self) -> BaseMessage | None:
-        frame = await self.transport.read_frame()
+        frame = await self._read_frame()
         try:
             message = self.decoder.decode(frame)
         except Exception as e:
@@ -28,10 +29,19 @@ class BaseProtocolSession:
 
         return message
 
+    async def _read_frame(self) -> bytes:
+        raw_header = await self.transport.read(HEADER_LENGTH)
+
+        raw_payload_length = FrameDecoder.extract_payload_length(raw_header)
+
+        raw_payload = await self.transport.read(raw_payload_length)
+
+        return raw_header + raw_payload
+
     async def _write_message(self, response: BaseMessage) -> None:
         transaction_id = response.header.transaction_id
         logger.info(
             f"→ Send {response.header.function_code.name} ({response.header.function_code.value:#02x}) response, {transaction_id=}"
         )
         frame = self.encoder.encode(response)
-        await self.transport.write_frame(frame)
+        await self.transport.write(frame)
