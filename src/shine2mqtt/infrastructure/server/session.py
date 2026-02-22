451e6ab046bf188f4d4453a05a74132e1d48278b
bremme <6715707+bremme@users.noreply.github.com>
2026-02-22 22:05:46 +0100

    Remove protocol dependency

--- src/shine2mqtt/infrastructure/server/session.py
+++ src/shine2mqtt/infrastructure/server/session.py
@@ -1,29 +1,21 @@
 from asyncio import StreamReader, StreamWriter
 
-from shine2mqtt.protocol.frame.constants import HEADER_LENGTH
-from shine2mqtt.protocol.frame.decoder import FrameDecoder
 from shine2mqtt.util.logger import logger
 
 
 class TCPSession:
     def __init__(self, reader: StreamReader, writer: StreamWriter):
-        self.reader = reader
-        self.writer = writer
+        self._reader = reader
+        self._writer = writer
 
-    async def read_frame(self) -> bytes:
-        raw_header = await self.reader.readexactly(HEADER_LENGTH)
+    async def read(self, num_bytes: int) -> bytes:
+        return await self._reader.readexactly(num_bytes)
 
-        raw_payload_length = FrameDecoder.extract_payload_length(raw_header)
-
-        raw_payload = await self.reader.readexactly(raw_payload_length)
-
-        return raw_header + raw_payload
-
-    async def write_frame(self, frame: bytes):
-        self.writer.write(frame)
-        await self.writer.drain()
+    async def write(self, frame: bytes):
+        self._writer.write(frame)
+        await self._writer.drain()
 
     async def close(self):
         logger.info("Closing TCP session")
-        self.writer.close()
-        await self.writer.wait_closed()
+        self._writer.close()
+        await self._writer.wait_closed()
